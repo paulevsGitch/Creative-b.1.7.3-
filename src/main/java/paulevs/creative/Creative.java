3e71959ea6aeeab80fcc27e88a22b21136b9e666
paulevsGitch <paulevs@yandex.ru>
2021-04-08 14:06:58 +0300

    Added jitpack

--- src/main/java/paulevs/creative/Creative.java
+++ src/main/java/paulevs/creative/Creative.java
@@ -1,25 +1,10 @@
 package paulevs.creative;
 
-import net.minecraft.block.BlockBase;
-import net.minecraft.item.ItemBase;
-import net.minecraft.item.ItemInstance;
 import net.modificationstation.stationloader.api.common.mod.StationMod;
-import paulevs.creative.api.SimpleTab;
-import paulevs.creative.api.TabRegister;
-import paulevs.creative.api.CreativeTabs;
 
 public class Creative implements StationMod {
 	public static final String MOD_ID = "creative";
 	
 	@Override
-	public void preInit() {
-		TabRegister.EVENT.register(new TabRegister() {
-			@Override
-			public void registerTabs() {
-				SimpleTab tab = CreativeTabs.register(new SimpleTab("test", "coolmod", ItemBase.string));
-				tab.addItem(new ItemInstance(ItemBase.stonePickaxe));
-				tab.addItem(new ItemInstance(BlockBase.BEDROCK));
-			}
-		});
-	}
+	public void preInit() {}
 }
